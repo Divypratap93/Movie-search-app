e03aa503a1dd128b07a93eb5ad420af9327bea72
Divy pratap <181524423+Divypratap93@users.noreply.github.com>
2025-01-20 11:05:02 +0000

    Key_protected

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { meta } from "@eslint/js";
 import {useState} from "react";
 
 export default function App(){
@@ -10,7 +11,7 @@ export default function App(){
     const searchMovies = async (e) => {
         e.preventDefault();
                 
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const res = await fetch(url);
